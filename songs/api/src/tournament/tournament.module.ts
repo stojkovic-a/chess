e86cc97d0b3b01a38555a2770814c67583473848
Aleksandar Stojkovic <aleksandarstojkovic@elfak.rs>
2023-09-07 17:31:10 +0200

    backend redo

--- songs/api/src/tournament/tournament.module.ts
+++ songs/api/src/tournament/tournament.module.ts
@@ -3,10 +3,16 @@ import { TournamentController } from './tournament.controller';
 import { TournamentService } from './tournament.service';
 import { Tournament } from './models/tournament.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from 'src/user/models';
+import { Game } from 'src/game/models';
 
 @Module({
-  imports:[TypeOrmModule.forFeature([Tournament])],
+  imports: [
+    TypeOrmModule.forFeature([Tournament]),
+    TypeOrmModule.forFeature([User]),
+    TypeOrmModule.forFeature([Game],)
+  ],
   controllers: [TournamentController],
   providers: [TournamentService]
 })
-export class TournamentModule {}
+export class TournamentModule { }
